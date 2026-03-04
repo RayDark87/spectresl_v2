d1374863c68baa5a1a45a653e28e5e73cb67bb69
hodeiprieto <hodei.prieto06@somo.eus>
2026-03-04 02:10:54 +0100

    Update post.php

--- post.php
+++ post.php
@@ -22,15 +22,60 @@
             $stmt = $conexion->prepare($sql);
 
             if ($stmt->execute([$nombre, $email, $tipo, $asunto, $mensaje])) {
-                echo "        body{
+                echo '<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro realizado</title>
+</head>
+<body>
+
+    <style>
+        body{
             font-family: Arial, Helvetica, sans-serif;
             background: radial-gradient(#111827, #0a0d14);
             color:#eef1f6;
             line-height:1.6;
-        }"
-                echo "Registro guardado correctamente en la nube.<br><br>";
-                echo "<a href='FORM2.html'>Volver</a> | ";
-                echo "<a href='LISTARSUGERENCIAS.php'>Ver registros</a>";
+            text-align: center;
+        }
+
+        
+        button{
+            padding:14px 28px;
+            border-radius:12px;
+            border:none;
+            background:linear-gradient(135deg,#c9b27a,#9fb3d1);
+            font-weight:700;
+            transition:.3s ease;
+        }
+
+        button:hover{
+            transform:translateY(-3px);
+            box-shadow:0 10px 25px rgba(201,178,122,0.4);
+        }
+
+        a{
+           color:#0a0d14;
+           text-decoration: none;
+           cursor: pointer;
+        }
+
+
+
+    </style>
+
+    <h2>Registro realizado correctamente</h2>
+    <p>Gracias por apoyarnos ;)</p>
+    <button>
+        <a href="/index.html">VOLVER AL INICIO</a>
+    </button><br><br>
+    <button>
+        <a href="/HTML_CSS/FORMULARIO.html">REGISTRAR OTRO USUARIO</a>
+    </button>
+    
+</body>
+</html>'
             }
         } catch (PDOException $e) {
             echo "Error al insertar en Postgres: " . $e->getMessage();
